7cb5865cd026c4b189dfcd8db577e6fd19435540
Eduardo Bacchiega <dubacchiega@gmail.com>
2025-03-21 13:28:26 -0300

    commit de estudos para mongodb e redis

--- src/main/java/com/bacchiega/basketservice/client/response/PlatziProductResponse.java
+++ src/main/java/com/bacchiega/basketservice/client/response/PlatziProductResponse.java
@@ -3,5 +3,6 @@ package com.bacchiega.basketservice.client.response;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+// Serializable é usado para poder manter algo em cache
 public record PlatziProductResponse(Long id, String title, BigDecimal price) implements Serializable {
 }
